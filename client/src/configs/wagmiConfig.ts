f6430102b526c9013962af0db9b11371959c95b8
Dev0001-1 <devrajsinghh1729@gmail.com>
2026-05-31 00:02:19 +0530

    DashBoard

--- client/src/configs/wagmiConfig.ts
+++ client/src/configs/wagmiConfig.ts
@@ -1,10 +1,9 @@
-
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { mainnet, sepolia } from "wagmi/chains";
 
 export const config = getDefaultConfig({
   appName: "CarbonX",
-  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "00000000000000000000000000000000",
+  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "37e0ba054fb6add85058f05d1484f891",
   chains: [mainnet, sepolia],
   ssr: true,
 });
